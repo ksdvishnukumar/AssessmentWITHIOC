977628b014ef512a96e20b430dea5089a8f117f0
VISHNU KUMAR K S D <ksdvishnukumar@gmail.com>
2018-12-19 00:31:05 +0530

    Committed With IOC Container

---
+++ PatientDemographicsSolution/PDAPI/DTO/DBEntity.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace PDAPI.DTO
+{
+    //DBOperation implmentation
+    public partial class DBEntity : IEntity
+    {
+        PatientDemographicsEntities p;
+        public DBEntity()
+        {
+            p = new PatientDemographicsEntities();
+        }
+
+        //Listing all the Persons
+        public List<PersonData> GetList()
+        {
+            return p.PersonDatas.ToList();
+        }
+
+        //Adding the new Person to the DB
+        public int Add(PersonData data)
+        {
+           PersonData pd = p.PersonDatas.Add(data);
+            if (pd != null)
+                return 1;
+            return 0;
+        }
+
+        //T0 Update the state of the existing entity that going to be modified
+        public void Update(PersonData data)
+        {
+            p.Entry(data).State = EntityState.Modified;
+        }
+
+        //Commiting the Modification to the DB
+        public int SaveChanges()
+        {
+            return p.SaveChanges();
+        }
+    }
+}
---
+++ PatientDemographicsSolution/PDAPI/DTO/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace PDAPI.DTO
+{
+    //This Interface is inherited by Both InMemoryEntity and DBEntity to remove the dependancy from the DBAccess layer.
+    public interface IEntity
+    {
+        List<PersonData> GetList();
+        int Add(PersonData p);        
+        void Update(PersonData p);
+        int SaveChanges();
+    }
+}
